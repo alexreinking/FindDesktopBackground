a472e22fe2d4fee11a13239be58e37ca534ca2c5
Alex Reinking <alex.reinking@gmail.com>
2018-07-07 14:06:27 -0700

    Add project files.

---
+++ DesktopBackground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesktopBackground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
